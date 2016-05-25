7edfcc6d12cce08ddb314768e8ec956422b21843
Mac <Mac@mavericks-5.local>
2016-05-25 15:52:12 -0400

    Initial Commit

---
+++ SkutCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SkutCalculator
+//
+//  Created by Mac on 5/25/16.
+//  Copyright © 2016 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
